86abea0c5dee17337ba45fa952f98edba67ccf82
Clayton Orman <corman.dev@gmail.com>
2017-06-26 16:17:14 -0400

    Add firebase integration to app

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,29 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-
 import { AppModule } from "./app.module";
+
+import { Config } from "./shared/config";
+import firebase = require("nativescript-plugin-firebase");
+
+firebase.init({
+    persist: false,
+    storageBucket: 'gs://uncorkd-cbf45.appspot.com',
+    onAuthStateChanged: (data: any) => {
+        console.log(`Auth State Changed: ${JSON.stringify(data)}`);
+        if (data.loggedIn) {
+            Config.token = data.user.uid;
+        }
+        else {
+            Config.token = '';
+        }
+    }
+}).then(
+    function(instance) {
+        console.log('firebase.init done');
+    },
+    function(error) {
+        console.log(`firebase.init error ${error}`);
+    }
+);
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,3 +1,4 @@
 export class Config {
     static apiUrl = 'https://uncorkd-cbf45.firebaseio.com/';
+    static token = '';
 }
