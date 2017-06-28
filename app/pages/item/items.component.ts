901377bf85073a1b3adf733550729a2c1781648d
Clayton Orman <corman.dev@gmail.com>
2017-06-28 15:52:27 -0400

    Implement logout feature

--- app/pages/item/items.component.ts
+++ app/pages/item/items.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-
 import { Item } from "../../shared/models/item.model";
 import { ItemService } from "../../shared/services/item.service";
 import { BackendService } from "../../shared/services/backend.service";
+import { FirebaseService } from '../../shared/services/firebase.service';
+import { RouterExtensions } from 'nativescript-angular/router/router-extensions';
 
 @Component({
     selector: "ns-items",
-    providers: [ItemService],
+    providers: [ItemService, FirebaseService],
     moduleId: module.id,
     templateUrl: "./items.html",
     styleUrls: ["./items-common.css", "./items.css"]
@@ -14,10 +15,14 @@ import { BackendService } from "../../shared/services/backend.service";
 export class ItemsComponent implements OnInit {
     items: Item[];
 
-    constructor(private itemService: ItemService) { }
+    constructor(private itemService: ItemService, private fireBaseService: FirebaseService, private routerExtensions: RouterExtensions) { }
 
     ngOnInit(): void {
         this.items = this.itemService.getItems();
-        alert(`Is the user logged in: ${BackendService.isLoggedIn()}`);
+    }
+
+    logout() {
+        this.fireBaseService.logout();
+        this.routerExtensions.navigate(['login'], { clearHistory: true });        
     }
 }
